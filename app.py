817cc5f5dc3a2b82585407be7395ef6581fe18aa
Anilkanta-sudo <akanta895@gmail.com>
2020-07-06 14:59:11 +0530

    third build

--- app.py
+++ app.py
@@ -1,57 +1,27 @@
-from flask import Flask, request, redirect, render_template
+import os
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.multiclass import *
+from sklearn.svm import *
+import pandas
 
 app = Flask(__name__)
+global Classifier
+global Vectorizer
 
-# importing the Dataset
-import pandas as pd
+# load data
 
-messages = pd.read_csv('/home/smsc/Spam_classifier_App/Spam_classifier/smsspamcollection/SMSSpamCollection', sep='\t', names=["label", "message"])
+data = pandas.read_csv('/home/smsc/NLP/smsspamcollection/SMSSpamCollection', sep='\t', encoding='latin-1',
+                       names=["label", "message"])
+data.rename(columns={'label': 'v1', 'message': 'v2'}, inplace=True)
+train_data = data[:4500]
+test_data = data[4500:]
 
-# Data cleaning and preprocessing
-import re
-import nltk
-print(messages.describe())
-from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer
-from nltk.stem import WordNetLemmatizer
-
-ps = PorterStemmer()
-wordnet = WordNetLemmatizer()
-corpus = []
-for i in range(0, len(messages)):
-    review = re.sub('[^a-zA-Z]', ' ', messages['message'][i])
-    review = review.lower()
-    review = review.split()
-    review = [wordnet.lemmatize(word) for word in review if not word in set(stopwords.words('english'))]
-    review = ' '.join(review)
-    corpus.append(review)
-
-# creating Tf-IDF model
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-
-cv = CountVectorizer()
-x = cv.fit_transform(corpus).toarray()
-
-y = pd.get_dummies(messages['label'])
-# taking spam column values only
-y = y.iloc[:, 1].values
-
-# Train test split
-from sklearn.model_selection import train_test_split
-
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.20, random_state=0)
-
-# Training model using naive bayes classifier
-
-from sklearn.naive_bayes import MultinomialNB
-
-spam_detect_model = MultinomialNB().fit(x_train, y_train)
-y_pred = spam_detect_model.predict(x_test)
-
-# prediction with user text
-clf = MultinomialNB()
-clf.fit(x_train, y_train)
-clf.score(x_test, y_test)
+# train model
+Classifier = OneVsRestClassifier(SVC(kernel='linear', probability=True))
+Vectorizer = TfidfVectorizer()
+vectorize_text = Vectorizer.fit_transform(train_data.v2)
+Classifier.fit(vectorize_text, train_data.v1)
 
 
 @app.route('/')
@@ -64,12 +34,33 @@ def spam_page():
 def results_page():
     if request.method == "POST":
         message = request.form['content']
-        message = str(message).lower()
-        data = [message]
-        vect = cv.transform(data).toarray()
-        my_prediction = clf.predict(vect)
-        return render_template('results.html', my_prediction=my_prediction)
+        print(message)
+        error = ''
+        predict_proba = ''
+        predict = ''
+
+        global Classifier
+        global Vectorizer
+        try:
+            if len(message) > 0:
+                vectorize_message = Vectorizer.transform([message])
+                predict = Classifier.predict(vectorize_message)[0]
+                predict_proba = Classifier.predict_proba(vectorize_message).tolist()
+                ham_data = predict_proba[0][0]
+                ham_data = "{:.2f}".format(ham_data)
+                spam_data = predict_proba[0][1]
+                spam_data = "{:.2f}".format(spam_data)
+        except BaseException as inst:
+            error = str(type(inst).__name__) + ' ' + str(inst)
+
+        if predict == "spam":
+            return render_template('results.html', my_prediction=predict, spam_data=spam_data, ham_data=ham_data)
+        elif float(ham_data) < 0.7:
+            return render_template('results.html', my_prediction="spam", spam_data=spam_data)
+
+        else:
+            return render_template('results.html', my_prediction=predict, spam_data=spam_data, ham_data=ham_data)
 
 
-if __name__=="__main__":
-    app.run(host="192.168.74.51",port=8002, debug=True)
+if __name__ == "__main__":
+    app.run(host="192.168.74.51", port=8002, debug=True)
